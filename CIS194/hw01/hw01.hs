1adcdba61a92712e961272e23829afa568f63840
Ryan Castner <castner.rr@gmail.com>
2015-03-21 14:44:58 -0400

    changed toDigits to use guards for comprehension, removed old doubleEveryOther function

--- CIS194/hw01/hw01.hs
+++ CIS194/hw01/hw01.hs
@@ -1,22 +1,15 @@
 toDigits :: Integer -> [Integer]
-toDigits x = 	if x <= 0 then []
-		else helper x []
-			where	helper x [] = helper (div x 10) (mod x 10 : [])
+toDigits x	| x <= 0	= []
+		| otherwise 	= helper x []
+			where	helper x [] = helper (x `div` 10) (x `mod` 10 : [])
 				helper 0 xs = xs
-				helper x xs = helper (div x 10) (mod x 10 : xs)
+				helper x xs = helper (x `div` 10) (x `mod` 10 : xs) 
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev x = reverse (toDigits x)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther (x:[]) 	= 	[x]
-doubleEveryOther (x:y:[]) 	=	x * 2 : y : []
-doubleEveryOther (x:y:zs) 	= 	double (reverse(x:y:zs))
-					where double (x:y:zs) = reverse $ x : y*2 : reverse(doubleEveryOther (reverse zs))
-
-
-doubleEveryOther' :: [Integer] -> [Integer]
-doubleEveryOther' x = reverse $ doubleEveryOtherBeginning (reverse x)
+doubleEveryOther x = reverse $ doubleEveryOtherBeginning (reverse x)
 
 doubleEveryOtherBeginning :: [Integer] -> [Integer]
 doubleEveryOtherBeginning (x:y:zs) 	= x : y*2 : doubleEveryOtherBeginning zs
